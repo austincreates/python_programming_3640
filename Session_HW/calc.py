98c3154c0bd146df3721217e9e4ef2640a807da1
austincreates <apearl3@babson.edu>
2017-09-08 18:23:24 -0400

    correcting calc.py

--- Session_HW/calc.py
+++ Session_HW/calc.py
@@ -1,5 +1,9 @@
+#Assignment #1
+#Exercise 2.1
 print ((42*60) + 42, "seconds")
+#Exercise 2.2
 print (10/1.61, "miles")
+#Exercise 2.3
 #assuming we are only using skills learned in-class
 """ print ((42*60) + 42) #total seconds
 print (10/1.61) #amount of miles
@@ -10,9 +14,14 @@ print ("6 minute 52 second walking pace")
 """ print ((60*6) + 52) #seconds ran per mile
 print (60*60) #seconds ran per hour """
 print (3600/412, "miles per hour")
-pi = 3.14
-radius = 5
-print ((4/3)* pi * radius **3, "units^3") 
+
+#exercise 1.1
+pi = 3.14159265358997931
+r = 5
+v = 4/3*pi*r**3
+print ("The volume of a sphere with radius 5 is {:.2f}.".format(v)) 
+
+#exercise 1.2
 cp = 24.95 #coverprice
 dis = .4 #discount
 ship = 3 #shipping cost
@@ -20,21 +29,22 @@ add = .75 #cost of additional copy
 copy = 60 #copies purchased
 total = (copy*cp) * (1-dis) + 3 + (copy - 1) * add
 total = round (total, 2)
-print("$", total, "is total price of books")
-ep = (8 * 60) + 15 #easy pace in seconds
-tempo = 7 * 60 + 12 #tempo in seconds
-total = (2 * ep) + 3 * tempo
-#print (total)
-minutes = 2286/60
-#print (minutes)
-# 38 minutes
-seconds = .1 * 60
-#print (seconds)
-new_time_min = 52 + 38 - 60
-#print (new_time_min)
-new_time_min_secs = new_time_min + seconds/100
-print (new_time_min_secs)
-print("Therefore, new time is 7:30:06")
+print("$", total, "is total wholesale cost of books")
+
+#exercise 1.3
+start_time_hr = 6 + 52/60
+easy_pace_hr = (8 + 15/60)/60
+tempo_pace_hr = (7+ 12/60)/60
+running_time_hr = 2 * easy_pace_hr + 3 * tempo_pace_hr
+breakfast_hr = start_time_hr + running_time_hr
+breakfast_min = (breakfast_hr - int(breakfast_hr)) * 60
+breakfast_sec = (breakfast_min - int(breakfast_min)) * 60
+
+print('Breakfast time is {:02d}:{:02d}:{:02d}.'.format(
+    int(breakfast_hr),
+    int(breakfast_min),
+    int(breakfast_sec)))
+#exercise 1.4
 increase = (89/82 - 1) * 100
 #print (increase)
 print ("The percent increase of the average scores is approximately {:.1f}%." .format (increase))
