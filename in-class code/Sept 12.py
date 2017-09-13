f76bcf9d87058372def0b38b99cf7ce61596f826
austincreates <apearl3@babson.edu>
2017-09-12 22:29:08 -0400

    fixed

---
+++ in-class code/Sept 12.py
@@ -0,0 +1,17 @@
+def print_lyrics():
+    print("Hey Jude. Don't make it bad.")
+    print("Take a sad song and make it better.")
+
+print_lyrics()
+print(type(print_lyrics))
+
+def repeat_lyrics():
+    print (print_lyrics)
+    print('Na - na - na - na - na, na - na - na - na')
+    print (print_lyrics)
+
+print(repeat_lyrics)
+
+def print_twice(whatever_name):
+    print(whatever_name)
+    print(whatever_name)
